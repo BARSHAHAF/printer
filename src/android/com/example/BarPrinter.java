949342242768c950bed3fbcb68de3ad40d762ab3
BARSHAHAF <b.shahaf@gmail.com>
2017-09-17 19:02:29 +0300

    Update BarPrinter.java

--- src/android/com/example/BarPrinter.java
+++ src/android/com/example/BarPrinter.java
@@ -12,6 +12,29 @@ import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.CancellationSignal;
+import android.os.ParcelFileDescriptor;
+import android.os.StrictMode;
+import android.print.PageRange;
+import android.print.PrintAttributes;
+import android.print.PrintDocumentAdapter;
+import android.print.PrintDocumentInfo;
+import android.print.PrintManager;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+
+
+
+
+
+
 import android.util.Log;
 
 import java.util.Date;
@@ -28,6 +51,85 @@ public class BarPrinter extends CordovaPlugin {
   public String PrintPdf(String file)
   {
   
+    
+    Context mContext=this.cordova.getActivity().getApplicationContext();
+    
+    
+    final String[] print = {"http://5.100.254.203/~promo/test.pdf"};
+
+                PrintDocumentAdapter pda = new PrintDocumentAdapter(){
+
+
+
+                    @Override
+                    public void onWrite(PageRange[] pages, ParcelFileDescriptor destination, CancellationSignal cancellationSignal, WriteResultCallback callback){
+                        InputStream input = null;
+                        OutputStream output = null;
+
+                        try {
+
+                            //  input = new FileInputStream("http://5.100.254.203/~promo/test.pdf");
+                            input = new URL(print[0]).openStream();
+                            output = new FileOutputStream(destination.getFileDescriptor());
+
+                            byte[] buf = new byte[1024];
+                            int bytesRead;
+
+                            while ((bytesRead = input.read(buf)) > 0) {
+                                output.write(buf, 0, bytesRead);
+                            }
+
+                            callback.onWriteFinished(new PageRange[]{PageRange.ALL_PAGES});
+
+                        } catch (FileNotFoundException ee){
+                            //Catch exception
+                            Log.e("bar",ee.getMessage());
+                        } catch (Exception e) {
+                            Log.e("bar",e.toString());
+                            //Catch exception
+                        } finally {
+
+                  /*  try {
+                       // input.close();
+                       // output.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    */
+                        }
+                    }
+
+                    @Override
+                    public void onLayout(PrintAttributes oldAttributes, PrintAttributes newAttributes, CancellationSignal cancellationSignal, LayoutResultCallback callback, Bundle extras){
+
+                        if (cancellationSignal.isCanceled()) {
+                            callback.onLayoutCancelled();
+                            return;
+                        }
+
+
+                        PrintDocumentInfo pdi = new PrintDocumentInfo.Builder("Name of file").setContentType(PrintDocumentInfo.CONTENT_TYPE_DOCUMENT).build();
+
+                        callback.onLayoutFinished(pdi, true);
+                    }
+                };
+
+
+
+
+
+                Snackbar.make(v, "bar shahaf !! ", Snackbar.LENGTH_LONG)
+              
+                 PrintManager printManager = (PrintManager) mContext.getSystemService(Context.PRINT_SERVICE);
+                String jobName = " Document";
+                printManager.print(jobName, pda, null);
+    
+    
+    
+    
+    
+    
+    
   return file+" . got It";
   
   }
