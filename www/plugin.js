f943ad90050d76c590c28e3bc3d21d0550674511
BARSHAHAF <b.shahaf@gmail.com>
2017-09-17 18:46:54 +0300

    Update plugin.js

--- www/plugin.js
+++ www/plugin.js
@@ -4,6 +4,9 @@ var exec = require('cordova/exec');
 var PLUGIN_NAME = 'BarPrinter';
 
 var BarPrinter = {
+   PrintPdf: function(phrase, cb,ecb) {
+    exec(cb, ecb, PLUGIN_NAME, 'PrintPdf', [phrase]);
+  },
   echo: function(phrase, cb,ecb) {
     exec(cb, ecb, PLUGIN_NAME, 'echo', [phrase]);
   },
